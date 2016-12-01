e6e91471f85694d21a539d879cc4dc9617674e01
Paweł Korus <pkorus@agh.edu.pl>
2016-12-01 12:07:35 +0800

    Changed stride configuration parameter

--- detectors/detectForgeryPRNUMultiscale.m
+++ detectors/detectForgeryPRNUMultiscale.m
@@ -1,4 +1,4 @@
-function detection_results = detectForgeryPRNUMultiscale(image, camera_model, scales, overlap, detector, verbose)
+function detection_results = detectForgeryPRNUMultiscale(image, camera_model, scales, stride, detector, verbose)
 % detection_results = detectForgeryPRNUMultiscale(image, camera_model, scales, overlap, detector, verbose)
 % 
 % Wrapper function that unifies various localization variants with a single API.
@@ -7,13 +7,20 @@ function detection_results = detectForgeryPRNUMultiscale(image, camera_model, sc
 %   - image         - input image, either uint8 RGB or string with image path
 %   - camera_model  - structure with camera's PRNU model (see trainCameraModel)
 %   - scales        - desired scales of analysis, e.g., [65, 97, 129]
-%   - overlap       - sliding window overlap:
-%                     0   - no overlap, windows next to each other
-%                     0.5 - half overlap, stride = half window size
-%                     1   - full overlap, stride = 8 px
+%   - stride        - sliding window stride, fractional values can be used to
+%                     use stride relative to window size, e.g.:
+%                       >=1  - use directly as stride
+%                       0.5 - stride = half window size
+%                       0   - no overlap, windows next to each other
+%                     Note:
+%                      - values will be floored to the powers of 2
+%                      - for FWA analysis, minimal stride is 8
+%                        (localization resolution)
 %   - detector      - analysis strategy:
-%                     'standard'  - standard implementation with central-pixel attribution
-%                     'blockwise' - alternative implementation with full-window attribution
+%                     'standard'  - standard implementation with 
+%                                   central-pixel attribution (CPA)
+%                     'blockwise' - alternative implementation with
+%                                   full-window attribution (FWA)
 %                     'central'   - faster implementation based on image filtering, should be roughly equivalent to 'standard'
 %                     'boxcar'    - same as 'central' but the correlation field will undergo an additional boxcar filtering
 %                     'guided'    - same as 'central' but used guided filtering instead of a box filter
@@ -40,21 +47,21 @@ function detection_results = detectForgeryPRNUMultiscale(image, camera_model, sc
     end
     parfor si = 1:numel(scales)
         s = scales(si);
-        if overlap >= 1
-            stride = 8 * round(overlap);
+        if stride >= 1
+            local_stride = round(stride);
         else
-            stride = 64 * max(1, round((1-overlap)*floor(s/8)));
+            local_stride = max(1, power(2,floor(log2((1-stride)*s))));
         end
         if strfind('blockwise', detector) == 1        
-            maps{si} = detectForgeryPRNU(image, camera_model, s, stride, verbose);
+            maps{si} = detectForgeryPRNU(image, camera_model, s, max(8, local_stride), verbose);
         elseif strfind('boxcar', detector) == 1
-            maps{si} = detectForgeryPRNUFilter(image, camera_model, s, struct('stride', stride, 'rates', 0.01, 'mode', 'b', 'verbose', verbose));
+            maps{si} = detectForgeryPRNUFilter(image, camera_model, s, struct('stride', local_stride, 'rates', 0.01, 'mode', 'b', 'verbose', verbose));
         elseif strfind('guided', detector) == 1
-            maps{si} = detectForgeryPRNUFilter(image, camera_model, s, struct('stride', stride, 'rates', 0.01, 'mode', 'g', 'verbose', verbose));
+            maps{si} = detectForgeryPRNUFilter(image, camera_model, s, struct('stride', local_stride, 'rates', 0.01, 'mode', 'g', 'verbose', verbose));
         elseif strfind('central', detector) == 1
-            maps{si} = detectForgeryPRNUFilter(image, camera_model, s, struct('stride', stride, 'rates', 0.01, 'mode', 'c', 'verbose', verbose));
+            maps{si} = detectForgeryPRNUFilter(image, camera_model, s, struct('stride', local_stride, 'rates', 0.01, 'mode', 'c', 'verbose', verbose));
         elseif strfind('standard', detector) == 1
-            maps{si} = detectForgeryPRNUCentral(image, camera_model, s, struct('stride', stride, 'rates', 0.01, 'verbose', verbose, 'calc_pce', true));
+            maps{si} = detectForgeryPRNUCentral(image, camera_model, s, struct('stride', local_stride, 'rates', 0.01, 'verbose', verbose, 'calc_pce', true));
         else
             exception = MException('prnu:InvalidParameter', sprintf('ERROR Unknown detector (%s)!', detector));
             throw(exception);
